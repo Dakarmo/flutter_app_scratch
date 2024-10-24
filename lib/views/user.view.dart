b37abedcc5a8b16525eeb70d4bbe1950957d9ded
Armel K. Dovenon <dove.armel@gmail.com>
2024-10-24 11:37:45 +0100

    création de la barre de recherche sur la page userGithub et récupération dynamique de ce qui est entré dans le champs de recherche

---
+++ lib/views/user.view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+
+class UserView extends StatefulWidget {
+  
+  @override
+  State<UserView> createState() => _UserViewState();
+}
+
+class _UserViewState extends State<UserView> {
+ 
+ String query = '';
+
+  TextEditingController queryTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return   Scaffold(
+      // drawer: MyDrawer(),
+      appBar: AppBar(
+        title:  Text('Github User => $query'),
+      ),
+      body:   Center(
+        // child: Text(
+        //   'Github User', 
+        //   // style: TextStyle(color: Colors.green, fontSize: 26), 
+        //   style: Theme.of(context).textTheme.headlineMedium, 
+        // ),
+      child: Column(
+        children: [
+          Row(
+            
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        query=value;
+                      });
+                    },
+                    controller: queryTextEditingController,
+                    decoration: InputDecoration(
+                      // icon: const Icon(Icons.logout),
+                      suffixIcon: const Icon(Icons.visibility),
+                      contentPadding: const EdgeInsets.all(10),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: const BorderSide(
+                          width: 1, color: Colors.green,
+                        )
+                      )
+                    ),
+                    
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                   setState(() {
+                     query = queryTextEditingController.text;
+                   });
+                  
+                }, 
+                icon: const Icon(Icons.search, )
+              )
+            ],
+          )
+      ],
+      )
+
+      ),
+    );
+  }
+}
