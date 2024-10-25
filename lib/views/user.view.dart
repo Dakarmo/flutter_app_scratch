efa79644e5b2d45e92e01c6a00c39d5d47f733a0
Armel K. Dovenon <dove.armel@gmail.com>
2024-10-25 09:55:54 +0100

    envoie de request http pour la recherche d'un utilisateur github.

--- lib/views/user.view.dart
+++ lib/views/user.view.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class UserView extends StatefulWidget {
   
@@ -9,17 +11,38 @@ class UserView extends StatefulWidget {
 
 class _UserViewState extends State<UserView> {
  
- String query = '';
- bool notVisible = false;
-
+  String query = '';
+  bool notVisible = false;
   TextEditingController queryTextEditingController = TextEditingController();
+  dynamic data;
+  int currentPage=0;
+  int totalPage=0;
+  int pageSize=20;
 
+void _search(String query) {
+    final url = Uri.parse("https://api.github.com/search/users?q=$query&per_page=$pageSize&page=$totalPage");
+    http.get(url)
+      .then((response) {
+        setState(() {
+          data = json.decode(response.body);
+          if(data['total_count'] % pageSize == 0){
+
+            totalPage = data['total_count']~/pageSize; 
+          }else{
+            totalPage = (data['total_count']~/pageSize )+ 1; 
+          }
+        });
+      })
+      .catchError((err){
+        print(err);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
       // drawer: MyDrawer(),
       appBar: AppBar(
-        title:  Text('Github User => $query'),
+        title:  Text('Github User => $query ||  Pagination :  $currentPage / $totalPage'),
       ),
       body:   Center(
         // child: Text(
@@ -72,12 +95,42 @@ class _UserViewState extends State<UserView> {
                 onPressed: () {
                    setState(() {
                      query = queryTextEditingController.text;
+                     _search(query);
                    });
                   
                 }, 
                 icon: const Icon(Icons.search, )
               )
             ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: (data==null) ? 0 : data['items'].length,
+              itemBuilder: (context, index) {
+              
+                return ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(data['items'][index]['avatar_url']),
+                            radius: 20,
+                          ),
+                          const SizedBox(width: 10, ),
+                          Text("${data['items'][index]['login']}"),
+                        ],
+                      ),
+                      CircleAvatar(
+                        child: Text("${data['items'][index]['score']}"),
+                            // radius: 10,
+                      )
+                    ],
+                  ),
+                );
+                      
+              }),
           )
       ],
       )
@@ -85,4 +138,6 @@ class _UserViewState extends State<UserView> {
       ),
     );
   }
+  
+  
 }
